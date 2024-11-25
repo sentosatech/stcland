f2e97c6af3174bf0208a9b1c2322ddb1f6b11acf
Maria Torrente <mctorrentedev@gmail.com>
2024-11-25 08:09:01 -0500

    109-rdc: minor enhacenments in select done.

--- packages/components/src/select/Select.tsx
+++ packages/components/src/select/Select.tsx
@@ -46,10 +46,10 @@ const Select: React.FC<SelectProps> = ({
   const selectStyles = {
     root: 'relative w-64',
     label: 'block mb-2 text-sm font-medium text-gray-700',
-    button: 'flex flex-row items-center justify-between w-full px-4 py-2 text-left bg-neutral-900 border border-gray-400 text-neutral-400 rounded focus:outline-none',
+    button: 'flex flex-row items-center justify-between w-full px-4 py-2 text-left bg-neutral-900 border border-gray-400 text-neutral-400 rounded focus:outline-none cursor-pointer',
     menu: 'absolute z-10 w-full bg-zinc-700 border border-gray-300 text-neutral-400 rounded shadow-lg max-h-60 overflow-auto',
     disabled: 'disabled:bg-gray-400',
-    optionContainer: 'flex items-center p-2',
+    optionContainer: 'flex items-center p-2 cursor-pointer',
     listItem: {
       base: 'ml-10',
       selected: 'ml-0',
